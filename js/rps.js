5eda6598652d49ef839a4da6ded8c3166f441f2e
Wei <chongweikang5@gmail.com>
2021-12-24 22:48:26 +0800

    Hello

--- js/rps.js
+++ js/rps.js
@@ -1,104 +1,104 @@
-const array = ["Rock","Paper","Scissors"];
-let comp_result = "a";
-
+let playerScore = 0;
+let computerScore = 0;
+const btns = document.querySelectorAll(".btn");
 // Get a random result for the computer 
 
-function computerPlay(arr){
-    index = Math.floor(Math.random() * arr.length);
-    result = arr[index];
-    // console.log(typeof result);
+function computerPlay(){
+    choice = ["rock","paper","scissors"];
+    index = Math.floor(Math.random() * choice.length);
+    result = choice[index];
+
     return result;
+}
+
+function disableButtons(){
+    btns.forEach(elem => {
+        elem.disabled = true;
+    })
 }
 
 // Determine the winner for 1 round
 
-function playRound(playerSelection,computerSelection){
-    // Default let result = 0 which means player loses.
-    // Result = 1 means player wins.
-    // Result = 2 means player & computer ties.
-    let res = 0;
+function playRound(playerSelection){
+    let res = "";
+    let computerSelection = computerPlay();
+    if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")){
+            playerScore++;
+        }
+        res = `You win! ${playerSelection} beats ${computerSelection}<br><br>
+                Player score: ${playerScore}<br>
+                Computer score: ${computerScore}`;
+        if (playerScore === 5){
+            disableButtons();
+            res += "<br><br>You win! Reload the page to play again!";
+        }
 
-    if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper"){
-        alert("You Lose!\nPaper (Com) beats Rock (You)");
-        return res; 
-    }
-    else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors"){
-        alert("You Win!\nRock (You) beats Scissors (Com)");
-        res = 1;
-        return res;
-    }
-    else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock"){
-        alert("Tie! Both are rocks");
-        res = 2;
-        return res;
-    }
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper"){
-        alert("Tie! Both are paper");
-        res = 2;
-        return res;
-    }
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"){
-        alert("You Win!\nPaper (You) beats Rock (Com)");
-        res = 1;
-        return res;
-    }
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors"){
-        alert("You Lose!\nScissors (Com) beats Paper (You)");
-        return res;
-    }
-    else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper"){
-        alert("You Win!\nScissors (You) beats Paper! (Com) ");
-        res = 1;
-        return res;
-    }
-    else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "scissors"){
-        alert("Tie! Both are scissors");
-        res = 2;
-        return res;
+    else if (playerSelection === computerSelection){
+        res = `It's a tie! You both chose ${playerSelection}<br>
+                Player score: ${playerScore}<br>
+                Computer score: ${computerScore}`;
     }
     else{
-        alert("You Lose!\nRock (Com) beats Scissors (You)");
-        return res;
+        computerScore++;
+        res = `You lose! ${playerSelection} beats ${computerSelection}<br><br>
+        Player score: ${playerScore}<br>
+        Computer score: ${computerScore}`;
+
+        if (computerScore === 5){
+            disableButtons();
+            res += "<br><br>You lose! Reload the page to play again!";
+        }
     }
+    document.getElementById("result").innerHTML = res;
+    return ;
 }
+
+btns.forEach(btn => {
+    btn.addEventListener("click",function(){
+        playRound(btn.value);
+    })
+})
 
 // const playerSelection = "rock";
 // let computerSelection = computerPlay(array);
 // console.log(computerSelection);
 // console.log(playRound(playerSelection,computerSelection));
 
-function game(){
-    let win = 0;
-    let lose = 0;
-    let tie = 0;
-    for (let i = 1; i<=5;i++){
-        const playerSelection = prompt("Please input your choice of rock, paper or scissors!");
-        const computerSelection = computerPlay(array);
-        result = playRound(playerSelection,computerSelection);
-        switch(result){
-            case 0:
-                lose++;
-                break;
-            case 1:
-                win++;
-                break;
-            case 2:
-                tie++;
-                break;
-        }
-    }
+// function game(){
+//     let win = 0;
+//     let lose = 0;
+//     let tie = 0;
+//     for (let i = 1; i<=5;i++){
+//         const playerSelection = prompt("Please input your choice of rock, paper or scissors!");
+//         const computerSelection = computerPlay(array);
+//         result = playRound(playerSelection,computerSelection);
+//         switch(result){
+//             case 0:
+//                 lose++;
+//                 break;
+//             case 1:
+//                 win++;
+//                 break;
+//             case 2:
+//                 tie++;
+//                 break;
+//         }
+//     }
 
-    if (win > lose){
-        alert(`You WIN!! \nYour record was ${win} wins, ${lose} losses and ${tie} ties.`);
-    }
+//     if (win > lose){
+//         alert(`You WIN!! \nYour record was ${win} wins, ${lose} losses and ${tie} ties.`);
+//     }
 
-    else if(lose > win){
-        alert(`You LOSE!! \nYour record was ${win} wins, ${lose} losses and ${tie} ties.`);
-    }
-    
-    else{
-        alert(`You TIED!! \nYour record was ${win} wins, ${lose} losses and ${tie} ties.`);
-    }
-}
+//     else if(lose > win){
+//         alert(`You LOSE!! \nYour record was ${win} wins, ${lose} losses and ${tie} ties.`);
+//     }
 
-game();
+//     else{
+//         alert(`You TIED!! \nYour record was ${win} wins, ${lose} losses and ${tie} ties.`);
+//     }
+// }
+
+// game();
+
